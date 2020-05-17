3fc4f4f1b3ecb0e6dcfe81b4a78ba963d450b7f3
shekhar agrawal <shekhar2010us@gmail.com>
2020-05-16 20:40:47 -0500

    Update server.rb

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -20,14 +20,14 @@ when "centos", "redhat", "amazon"
   end
 end
 
-#template '/var/www/html/index.html' do
-#  source 'index.html.erb'
-#end
-
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-    	<h2>PLATFORM: #{node['platform']}</h2>
-    	<h2>HOSTNAME: #{node['hostname']}</h2>
-	<h2>MEMORY:   #{node['memory']['total']}</h2>
-	<h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
+
+#file '/var/www/html/index.html' do
+#  content "<h1>Hello, world!</h1>
+#    	<h2>PLATFORM: #{node['platform']}</h2>
+#    	<h2>HOSTNAME: #{node['hostname']}</h2>
+#	<h2>MEMORY:   #{node['memory']['total']}</h2>
+#	<h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
+#end
