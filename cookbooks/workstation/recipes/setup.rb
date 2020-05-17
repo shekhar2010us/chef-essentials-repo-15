6b3f4749c4a07a23035d43df1a3a713837402f58
shekhar agrawal <shekhar2010us@gmail.com>
2020-05-16 20:42:51 -0500

    Update setup.rb

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -3,27 +3,27 @@ package 'tree' do
 end
 
 
-file '/etc/motd' do
- content 'This is custom motd. Property of Chef...'
- mode '0644'
- owner 'root'
- group 'root'
-end
-
-
-#template '/etc/motd' do
-#  source 'motd.erb'
-#  mode '0644'
-#  owner 'root'
-#  group 'root'
+#file '/etc/motd' do
+# content 'This is custom motd. Property of Chef...'
+# mode '0644'
+# owner 'root'
+# group 'root'
 #end
+
+
+template '/etc/motd' do
+  source 'motd.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
 
 cookbook_file '/etc/motd2' do
   source 'motd.file'
 end
 
-remote_file '/etc/motd3' do
-  source 'https://docs.chef.io/resource_remote_file.html'
-end
+#remote_file '/etc/motd3' do
+#  source 'https://docs.chef.io/resource_remote_file.html'
+#end
 
 
